8bd8993216ef575cf6c754cc672dc83ae7e6f295
RayXaus <137321629+max69-cyber@users.noreply.github.com>
2025-05-08 20:56:24 +0500

    Добавьте файлы проекта.

---
+++ taskorganizer.client/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Text } from "@chakra-ui/react";
+
+function App() {
+    const [forecasts, setForecasts] = useState();
+
+    useEffect(() => {
+        populateWeatherData();
+    }, []);
+
+    const contents = forecasts === undefined
+        ? <Text className="text-2xl font-bold text-center mb-6">dwadawd</Text>
+        : <table className="table table-striped" aria-labelledby="tableLabel">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                {forecasts.map(forecast =>
+                    <tr key={forecast.date}>
+                        <td>{forecast.date}</td>
+                        <td>{forecast.temperatureC}</td>
+                        <td>{forecast.temperatureF}</td>
+                        <td>{forecast.summary}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+
+    return (
+        <div>
+            <h1 id="tableLabel">Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+        </div>
+    );
+    
+    async function populateWeatherData() {
+        const response = await fetch('weatherforecast');
+        if (response.ok) {
+            const data = await response.json();
+            setForecasts(data);
+        }
+    }
+}
+
+export default App;
